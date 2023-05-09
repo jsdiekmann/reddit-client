598c384baf6765b07d6c88ffaa20da7778907125
jsdiekmann <97804946+jsdiekmann@users.noreply.github.com>
2023-05-09 06:23:31 -0400

    Formatting update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import CardContainer from "./Components/Card/CardContainer";
-import SearchBar from "./Components/SearchBar/SearchBar";
-import { parser } from "./parser";
+import CardContainer from "./Components/Card/CardContainer.js";
+import SearchBar from "./Components/SearchBar/SearchBar.js";
+import { parser } from "./parser.js";
 
 const App = () => {
   const [error, setError] = useState("");
